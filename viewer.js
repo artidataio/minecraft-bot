55a82a8fe58cb26191ec2ee93cc480e24c28b5e6
Imaduddin Haetami <imaduddin.htm@gmail.com>
2023-08-16 06:14:26 +0700

    Remove dead code.

--- viewer.js
+++
@@ -1,4 +0,0 @@
-const { mineflayer: mineflayerViewer } = require("prismarine-viewer");
-bot.once("spawn", () => {
-  mineflayerViewer(bot, { port: 12345, firstPerson: true }); // port is the minecraft server port, if first person is false, you get a bird's-eye view
-});
